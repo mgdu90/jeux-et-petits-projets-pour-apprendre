b561b9487be193d8ad18dbbb4783f6db259e0af3
charlesganne <125387743+charlesganne@users.noreply.github.com>
2023-11-22 14:17:55 +0100

    Delete 2048 directory

--- 2048/duo.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "duo.h"
-
-void duo()
-{
-	printf("vous etes dans le mode duo\n");
-}
--- 2048/puzzle.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "puzzle.h"
-
-void puzzle()
-{
-	printf("vous etes dans le mode puzzle\n");
-}
